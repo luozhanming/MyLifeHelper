fc28f0e9f2773e32c011909e2780c8348ce97151
luozhanming <459557210@qq.com>
2019-11-18 23:32:31 +0800

    修改

--- github/src/main/java/cn/luozhanming/github/di/GithubModule.kt
+++ github/src/main/java/cn/luozhanming/github/di/GithubModule.kt
@@ -13,6 +13,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Named
 
 
 @Module
--- library/src/main/java/cn/luozhanming/library/common/AppConfig.kt
+++ library/src/main/java/cn/luozhanming/library/common/AppConfig.kt
@@ -4,12 +4,15 @@ import android.os.Environment
 
 object AppConfig {
     /**网络请求超时时间*/
-     val NET_TIME_OUT = 30L
+    val NET_TIME_OUT = 30L
 
     /**GithubApiURL*/
-     val GITHUB_BASE_URL = "https://api.github.com/graphql/"
+    val GITHUB_BASE_URL = "https://api.github.com/graphql/"
+
+    val GITHUB_URL = "https://github.com/"
     /**应用存储文件夹*/
-     val APP_EXTERN_PATH="${Environment.getExternalStorageDirectory().absolutePath}/MyLifeHelper/log"
+    val APP_EXTERN_PATH =
+        "${Environment.getExternalStorageDirectory().absolutePath}/MyLifeHelper/log"
 
     /**应用界面设计宽度基准，单位dp*/
     val DESIGN_WIDTH_BASE = 360
