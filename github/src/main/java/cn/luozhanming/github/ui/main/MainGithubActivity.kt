88de48ceb1a21cd8b8c85b59e7d8ec4531746137
luozhanming <459557210@qq.com>
2019-11-25 23:32:20 +0800

    修改

---
+++ github/src/main/java/cn/luozhanming/github/ui/main/MainGithubActivity.kt
@@ -0,0 +1,22 @@
+package cn.luozhanming.github.ui.main
+
+import cn.luozhanming.github.base.BaseGithubActivity
+import cn.luozhanming.github.databinding.FragmentGithubWelcomeBinding
+
+class MainGithubActivity:BaseGithubActivity<FragmentGithubWelcomeBinding>() {
+    override fun initViewModel() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLayoutId(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun daggerInject() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
